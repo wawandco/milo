b6b37df3c2e2b09daa8b7a4549d8fc378aafe0f8
Tatan <wgarcia@wawand.co>
2020-07-27 15:58:08 -0500

    Attrs should not have whitestaces between name and value

---
+++ reviewers/att_no_white_spaces.go
@@ -0,0 +1,42 @@
+package reviewers
+
+import (
+	"bufio"
+	"io"
+	"regexp"
+	"strings"
+)
+
+type AttrNoWhiteSpaces struct{}
+
+func (at AttrNoWhiteSpaces) ReviewerName() string {
+	return "attribute/no-white-spaces"
+}
+
+func (at AttrNoWhiteSpaces) Review(path string, page io.Reader) ([]Fault, error) {
+	result := []Fault{}
+
+	var number int
+	var line string
+
+	scanner := bufio.NewScanner(page)
+	for scanner.Scan() {
+		number++
+
+		line = scanner.Text()
+		if strings.TrimSpace(line) == "" {
+			continue
+		}
+
+		exp := regexp.MustCompile(`[a-z\-]+(\s+\=\s*|\s*\=\s+)`)
+		if exp.MatchString(line) {
+			result = append(result, Fault{
+				Reviewer: at.ReviewerName(),
+				Line:     number,
+				Rule:     Rules["0019"],
+			})
+		}
+	}
+
+	return result, nil
+}
---
+++ reviewers/att_no_white_spaces_test.go
@@ -0,0 +1,87 @@
+package reviewers_test
+
+import (
+	"bytes"
+	"testing"
+	"wawandco/milo/reviewers"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_AttrNoWhiteSpaces_Review(t *testing.T) {
+	r := require.New(t)
+
+	reviewer := reviewers.AttrNoWhiteSpaces{}
+	tcases := []struct {
+		name    string
+		content string
+		err     error
+		faults  []reviewers.Fault
+	}{
+		{
+			name: "Correct",
+			content: `
+				<span class="font-18 font-weight-bold">First Name</span>
+				<p class="font-14 text-yellow">LastName</p>
+				<span class="font-12 text-muted font-weight-light">Status</span>
+			`,
+
+			faults: []reviewers.Fault{},
+		},
+		{
+			name: "line 3 class attr has whitespace",
+			content: `
+				<span class="font-18 font-weight-bold">First Name</span>
+				<p class ="font-14 text-yellow">LastName</p>
+				<span class="font-12 text-muted font-weight-light">Status</span>
+			`,
+
+			faults: []reviewers.Fault{
+				{
+					Reviewer: reviewer.ReviewerName(),
+					Line:     3,
+					Rule:     reviewers.Rules["0019"],
+				},
+			},
+		},
+		{
+			name: "line 3 class attr has whitespace and line 4 data-block has space as well",
+			content: `
+				<span class="font-18 font-weight-bold">First Name</span>
+				<p class ="font-14 text-yellow">LastName</p>
+				<span class="font-12 text-muted font-weight-light" data-block= "last-name">Status</span>
+			`,
+
+			faults: []reviewers.Fault{
+				{
+					Reviewer: reviewer.ReviewerName(),
+					Line:     3,
+					Rule:     reviewers.Rules["0019"],
+				},
+				{
+					Reviewer: reviewer.ReviewerName(),
+					Line:     4,
+					Rule:     reviewers.Rules["0019"],
+				},
+			},
+		},
+	}
+
+	for _, tcase := range tcases {
+		page := bytes.NewBufferString(tcase.content)
+		faults, err := reviewer.Review("something.html", page)
+
+		r.NoError(err, tcase.name)
+		r.Len(faults, len(tcase.faults), tcase.name)
+		if len(tcase.faults) == 0 {
+			continue
+		}
+
+		for i, tfault := range tcase.faults {
+			r.Equal(faults[i].Reviewer, tfault.Reviewer, tcase.name)
+			r.Equal(faults[i].Line, tfault.Line, tcase.name)
+			r.Equal(faults[i].Rule.Code, tfault.Rule.Code, tcase.name)
+			r.Equal(faults[i].Rule.Description, tfault.Rule.Description, tcase.name)
+		}
+	}
+}
