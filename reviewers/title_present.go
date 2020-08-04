55e7221c6e9d23311060cd6af929bddb565701ae
Antonio Pagano <apagano@wawand.co>
2020-08-04 17:11:54 -0500

    adding titlepresent cases

--- reviewers/title_present.go
+++ reviewers/title_present.go
@@ -1,7 +1,6 @@
 package reviewers
 
 import (
-	"bytes"
 	"io"
 	"path/filepath"
 	"strings"
@@ -25,8 +24,9 @@ func (doc TitlePresent) Review(path string, page io.Reader) ([]Fault, error) {
 	result := []Fault{}
 
 	var htmlTag *html.Token
-	var found bool
-	var startBuff []byte
+	var startTag *html.Token
+	var endTag *html.Token
+	var content string
 
 	z := html.NewTokenizer(page)
 	for {
@@ -36,31 +36,36 @@ func (doc TitlePresent) Review(path string, page io.Reader) ([]Fault, error) {
 		}
 
 		token := z.Token()
-		if token.DataAtom.String() == "html" {
-			htmlTag = &token
-			continue
+
+		switch tt {
+		case html.StartTagToken:
+			if htmlTag == nil && token.DataAtom.String() == "html" {
+				htmlTag = &token
+				continue
+			}
+
+			if token.DataAtom.String() == "title" {
+				startTag = &token
+			}
+		case html.EndTagToken:
+			if token.DataAtom.String() == "title" {
+				endTag = &token
+			}
+
+		case html.TextToken:
+			if htmlTag == nil {
+				continue
+			}
+
+			if startTag == nil || content != "" {
+				continue
+			}
+
+			content = strings.TrimSpace(string(z.Raw()))
 		}
-
-		if token.DataAtom.String() != "title" {
-			continue
-		}
-
-		if tt == html.StartTagToken {
-			startBuff = z.Buffered()
-			continue
-		}
-
-		between := bytes.ReplaceAll(startBuff, z.Buffered(), []byte{})
-		between = bytes.ReplaceAll(between, z.Raw(), []byte{})
-
-		if len(between) == 0 {
-			continue
-		}
-
-		found = true
 	}
 
-	if htmlTag == nil || found {
+	if htmlTag == nil || (content != "" && endTag != nil) {
 		return result, nil
 	}
 
