9a72cab59eae9316df536aaa6a0247a1be9d43a2
Tatan <wgarcia@wawand.co>
2020-09-07 15:23:08 -0500

    Add coments into reviewers

--- reviewers/inline_css.go
+++ reviewers/inline_css.go
@@ -6,16 +6,21 @@ import (
 	"github.com/wawandco/milo/external/html"
 )
 
+// InlineCSS is a reviewer that checks that all tags in HTML file do not have the style attribute.
 type InlineCSS struct{}
 
+// ReviewerName returns the reviewer name.
 func (css InlineCSS) ReviewerName() string {
 	return "css/inline"
 }
 
+// Accepts checks if the file can be reviewed.
 func (css InlineCSS) Accepts(path string) bool {
 	return true
 }
 
+// Review returns a list of faults for each tag that has the style attribute.
+// It returns faults if there are tags like <div ... style="color: red;">.
 func (css InlineCSS) Review(path string, page io.Reader) ([]Fault, error) {
 	result := []Fault{}
 	z := html.NewTokenizer(page)
--- reviewers/tag_lowercase.go
+++ reviewers/tag_lowercase.go
@@ -6,16 +6,21 @@ import (
 	"github.com/wawandco/milo/external/html"
 )
 
+// StyleTag is a reviewer that checks that all tags into the HTML file must be in lowercase.
 type TagLowercase struct{}
 
+// ReviewerName returns the reviewer name.
 func (t TagLowercase) ReviewerName() string {
 	return "tag/lowercase"
 }
 
+// Accepts checks if the file can be reviewed.
 func (t TagLowercase) Accepts(path string) bool {
 	return true
 }
 
+// Review returns a fault for each tag that is not in lowercase.
+// The faults will be added if the HTML file have tags like <DIV>, <Input>, etc.
 func (t TagLowercase) Review(path string, page io.Reader) ([]Fault, error) {
 	result := []Fault{}
 
