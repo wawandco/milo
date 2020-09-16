a2c3e537c299545514b0230934a931e033605eb8
Antonio Pagano <apagano@wawand.co>
2020-09-16 09:01:55 -0500

    adding coment on cliOutput

--- cmd/milo/review/flags.go
+++ cmd/milo/review/flags.go
@@ -3,7 +3,7 @@ package review
 import flag "github.com/spf13/pflag"
 
 var (
-	//
+	// cliOutput holds the passed output flag value and determines the selected output format.
 	cliOutput string
 )
 
